981c2406ac4b18b427092b58e7fdc1bc41d632b8
Gary Barker <gary.barker@gmail.com>
2011-10-05 22:55:36 +0100

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Tutorial::Application.routes.draw do
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
