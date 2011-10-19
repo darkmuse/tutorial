36e14f8aa0e74c973ad5dae01ccd9b80ffade1ac
Gary Barker <gary.barker@gmail.com>
2011-10-19 23:48:22 +0100

    we can sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Tutorial::Application.routes.draw do
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup',  :to => 'users#new'
+
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
 
   root :to => 'pages#home'
-
-#  get "pages/home"
-#  get "pages/contact"
-#  get "pages/about"
-#  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
